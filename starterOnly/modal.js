98a98874d48090a1a4cc1c11c63100e5e7789f61
1leodurand <1leodurand@gmail.com>
2022-05-26 22:58:22 +0200

    final

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,12 @@
+/*
+  1. Fermer la modale (avec le x et clique extérieur)
+  2. Implémenter entrées du formulaire
+  3.
+  4.
+  5.
+*/
+
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -20,4 +29,255 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal event
+/*
+1. Selectionner le bouton (x) (span, classe : close)
+2. Ajout de l'évenement Onclick sur boutton close
+3. On cache la modale lors du clic
+*/
 
+document.querySelector("span.close").addEventListener('click', closeModal);
+
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+/*
+(1) Lier les labels aux entrées dans le HTML en utilisant les attributs "for" et "id" dans le code existant. Corriger le code HTML quand nécessaire.
+(2) Utiliser du JavaScript pur (pas de jQuery) pour terminer le formulaire :
+
+Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
+Les données doivent être saisies correctement :
+(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+- ajout de required en html
+- minlength="2" en html (existant)
+(2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
+- ajout de required en html
+- minlength="2" en html
+(3) L'adresse électronique est valide.
+- type="email"
+- required
+(4) Pour le nombre de concours, une valeur numérique est saisie.
+- type="number"
+- required
+(5) Un bouton radio est sélectionné.
+- required
+(6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
+- required 
+Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
+
+Prenom 
+Id = first 
+Nom 
+Id = last
+Email 
+Id = email
+Date de naissance
+Id = birthdate
+Nombre concours 
+Id  = quantity
+Bouton radio 
+Id = location1
+Condition générale d'utilisation
+For = checkbox1
+*/
+/*
+let eMail = document.getElementById("email");
+
+eMail.addEventListener("keyup", function (event) {
+  if(eMail.validity.typeMismatch||eMail.validity.valueMissing) {
+    eMail.setCustomValidity("J'attend un e-mail, cher gamer!");
+  } else {
+    eMail.setCustomValidity("");
+  }
+});
+
+let birthDate = document.getElementById("birthdate");
+
+birthDate.addEventListener("keyup", function (event) {
+  if(birthDate.validity.typeMismatch||birthDate.validity.valueMissing) {
+    birthDate.setCustomValidity("J'attend une date de naissance, cher gamer!");
+  } else {
+    birthDate.setCustomValidity("");
+  }
+});
+
+let nombreConcours = document.getElementById("quantity");
+
+nombreConcours.addEventListener("keyup", function (event) {
+  if(nombreConcours.validity.typeMismatch||nombreConcours.validity.valueMissing) {
+    nombreConcours.setCustomValidity("J'attend votre nombre de participation au concours, cher gamer!");
+  } else {
+    nombreConcours.setCustomValidity("");
+  }
+});
+*/
+
+// prenom
+//nom
+//email
+
+const form = document.getElementById('formulaire');//la valeur ne varie pas, on utilise const
+
+const first = document.getElementById('first');
+const errorfirst = document.getElementById('errorfirst');
+
+const last = document.getElementById('last');
+const errorlast = document.getElementById('errorlast');
+
+const email = document.getElementById('email');
+const erroremail = document.getElementById('erroremail');
+
+const birthdate = document.getElementById('birthdate');
+const errorbirthdate = document.getElementById('errorbirthdate');
+
+const quantity = document.getElementById('quantity');
+const errorquantity = document.getElementById('errorquantity');
+
+let locationChecked = document.querySelector('input[name="location"]:checked');
+let errorlocation = document.getElementById('errorlocation');
+
+const checkbox1 = document.getElementById('checkbox1');
+const errorcheckbox1 = document.getElementById('errorcheckbox1');
+
+//prenom
+
+first.addEventListener("input", function (event) {
+  // Chaque fois que l'utilisateur saisit quelque chose
+  // on vérifie la validité du champ e-mail.
+  if (first.validity.valid) {
+    // S'il y a un message d'erreur affiché et que le champ
+    // est valide, on retire l'erreur
+    errorfirst.innerHTML = ""; // On réinitialise le contenu
+    errorfirst.className = "error"; // On réinitialise l'état visuel du message
+  }
+}, false);
+
+//nom
+
+last.addEventListener("input", function (event) {
+
+  if (last.validity.valid) {
+    errorlast.innerHTML = "";
+    errorlast.className = "error";
+  }
+}, false);
+
+//email
+
+email.addEventListener("input", function (event) {
+  if (email.validity.valid) {
+    erroremail.innerHTML = "";
+    erroremail.className = "error";
+  }
+  
+}, false);
+
+//Date de naissance
+
+birthdate.addEventListener("input", function (event) {
+  if (birthdate.validity.valid) {
+    errorbirthdate.innerHTML = ""; 
+    errorbirthdate.className = "error";
+  }
+  
+}, false);
+
+//Date de naissance
+
+quantity.addEventListener("input", function (event) {
+  if (quantity.validity.valid) {
+    errorquantity.innerHTML = "";
+    errorquantity.className = "error";
+  }
+  
+}, false);
+
+//Le tournoi de l'année choisi
+
+document.querySelectorAll('input[name="location"]').forEach(element => {
+  element.addEventListener('click', function (event){
+      errorlocation.innerHTML = "";
+      errorlocation.className = "error";
+      locationChecked=true;
+  })
+});;
+
+//Condition utilisation
+
+checkbox1.addEventListener("input", function (event) {
+  if (checkbox1.checked) {
+    errorcheckbox1.innerHTML = "";
+    errorcheckbox1.className = "error";
+  }
+}, false);
+
+//gestion des erreur
+
+form.addEventListener("submit", function (event) {
+  // Chaque fois que l'utilisateur tente d'envoyer les données
+  // on vérifie que le champ email est valide.
+  if (!email.validity.valid) {
+    // S'il est invalide, on affiche un message d'erreur personnalisé
+    erroremail.innerHTML = "J'attends une adresse e-mail correcte, cher gamer&nbsp;!";
+    erroremail.className = "error active";
+    // Et on empêche l'envoi des données du formulaire
+    event.preventDefault();
+  }
+  if (!first.validity.valid) {
+    errorfirst.innerHTML = "Tu t'appelles pas Voldemort, alors donnes ton prénom&nbsp;!";
+    errorfirst.className = "error active";
+    event.preventDefault();
+  }
+  if (!last.validity.valid) {
+    errorlast.innerHTML = "Arrêtes de te prendre pour celui-dont-on-ne-prononce-pas-le-nom&nbsp;!";
+    errorlast.className = "error active";
+    event.preventDefault();
+  }
+  if (!birthdate.validity.valid) {
+    errorbirthdate.innerHTML = "T'es pas un poisson pané alors donne ta date de fabrication&nbsp;!";
+    errorbirthdate.className = "error active";
+    event.preventDefault();
+  }
+  if (!quantity.validity.valid) {
+    errorquantity.innerHTML = "Avoue combien de tournois auquel tu as participé cher gamer&nbsp;!";
+    errorquantity.className = "error active";
+    event.preventDefault();
+  }
+  if (!locationChecked) {
+    errorlocation.innerHTML = "Un tournoi!";
+    errorlocation.className = "error active";
+    event.preventDefault();
+  } 
+  if (!checkbox1.checked) {
+    errorcheckbox1.innerHTML = "Cocher!";
+    errorcheckbox1.className = "error active";
+    event.preventDefault();
+  }
+  if (checkbox1.checked && locationChecked && quantity.validity.valid) {
+    displayConf();
+  }
+}, false);
+
+function displayConf() {
+  let modalBody = document.querySelector('.modal-body');
+  modalBody.innerHTML = "Merci pour votre inscription";
+let button = document.createElement('button');
+button.onclick = closeModal;
+
++classecss
+modalBody.appendChild(button)
+}
+
+//
+
+/*
+4 : 
+Merci ! Votre réservation a été reçue.
+
+*/
+/* 
+5: 
+- Image déformé -> object-fit: cover;
+- texte coupé -> word-break: break-word;
+*/
